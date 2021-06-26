1cc764daaa2ee0f1294c3de5605361383f45aa6d
mahak4983 <mahak4983@gmail.com>
2021-06-26 17:36:22 +0530

    add: About Us

---
+++ client/src/Components/About/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './About.module.css';
+import jett from '../../Images/jett.jpg'
+function About() {
+    return (
+        <div className={styles.About + ' row'} style={{ marginBottom: '0' }}>
+
+            <div className="col l1 s12"></div>
+            <div className="col s12 m4 l6">
+                <h1 style={{ margin: '0px' }} className={styles.h1}>About Us</h1>
+                <p className={styles.p}>
+                    Blend your style and experience on a global, competitive stage.
+                    You have 13 rounds to attack and defend your side using sharp gunplay and tactical abilities.
+                    And, with one life per-round, you'll need to think faster than your opponent if you want to survive.
+                    Take on foes across Competitive and Unranked modes as well as Deathmatch and Spike Rush.
+                </p>
+                <hr className={styles.hr} />
+            </div>
+            <div className={styles.x + " col s12 m5 l5"}>
+                <img className={styles.img} src={jett} alt="Jett OP" />
+            </div>
+        </div>
+    )
+}
+
+export default About
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import Section1 from '../Components/Section1/Section1';
+import About from '../Components/About/About';
 const Home = () => {
     return (
         <Fragment>
             <Section1 />
+            <About />
         </Fragment>
     );
 };
