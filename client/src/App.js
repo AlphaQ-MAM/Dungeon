e4016bd9ff7db043548fefdfe10a397e753afbba
Mahak Chhajer <61613948+mahak4983@users.noreply.github.com>
2021-06-26 17:49:54 +0530

    Add: Homepage Features (#5)
    * add: Homepage parallax
    
    * add: About Us

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Navigation/Navigation';
 import News from './Pages/News/News'
+import Home from './Pages/Home';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={Home} />
         <Route exact path="/news" component={News}/>
       </Switch>
     </div>
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Section1 from '../Components/Section1/Section1';
+import About from '../Components/About/About';
+const Home = () => {
+    return (
+        <Fragment>
+            <Section1 />
+            <About />
+        </Fragment>
+    );
+};
+export default Home;
