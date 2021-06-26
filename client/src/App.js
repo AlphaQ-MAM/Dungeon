bf636e19e3c56af2f3319f77045df77a6528f075
mahak4983 <mahak4983@gmail.com>
2021-06-26 12:52:38 +0530

    add: link router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Switch>
+        <Route exact path="/" />
+      </Switch>
     </div>
   );
 }
