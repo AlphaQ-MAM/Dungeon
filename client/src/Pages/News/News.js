2aa452a67cc5c4eb6c2dde16988f24f0901c131a
Manan <vora.manan10@gmail.com>
2021-06-27 11:53:47 +0530

    Merchandise Page Added

--- client/src/Pages/News/News.js
+++ client/src/Pages/News/News.js
@@ -67,7 +67,6 @@ export const News = () => {
                             </div>
                         </div>
                     </div>
-                    <hr />
                     <NewsContainer checked={checked} />
 
                 </div>
