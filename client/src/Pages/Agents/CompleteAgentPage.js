a14b13f384a72da594386675ef35312ef37d60c2
Anishwar <50guptaaniswar@gmail.com>
2021-06-27 18:10:45 +0530

    fixed dropdown

--- client/src/Pages/Agents/CompleteAgentPage.js
+++ client/src/Pages/Agents/CompleteAgentPage.js
@@ -1,32 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import AgentComp from "../../Components/Agent/AgentComp";
 function CompleteAgentPage(props) {
-
   const items = [
     { label: "OMEN", value: "OMEN" },
-    { label: "JETT", value: 'JETT' },
-    { label: "RAZE", value: 'RAZE' },
-    { label :"PHOENIX",value:"PHOENIX"}
+    { label: "JETT", value: "JETT" },
+    { label: "RAZE", value: "RAZE" },
+    { label: "PHOENIX", value: "PHOENIX" },
   ];
-  const[agent,setAgent]=useState("OMEN");
-  
+  const [agent, setAgent] = useState("OMEN");
+
   return (
-    <div >
-    <select
-      value={agent}
-      onChange={(e) =>setAgent(e.target.value)}
-    >{items.map(item => (
-      <option
-        key={item.value}
-        value={item.value}
+    
+    <div className="black" >
+      
+      <div style={{borderTop:"3px solid white"}}>
+      <select
+        className="white-text"
+        style={{ backgroundColor: "black" ,width:"50%",margin:"0 auto"}}
+        value={agent}
+        onChange={(e) => setAgent(e.target.value)}
       >
-        {item.label}
-        </option>
-         ))}
+        {items.map((item) => (
+          <option key={item.value} value={item.value}>
+            {item.label}
+          </option>
+        ))}
       </select>
-      <div><AgentComp agent={agent}/></div>
-       
+      </div>
+      <div>
+        <AgentComp agent={agent} />
+      </div>
     </div>
+  
   );
 }
 
